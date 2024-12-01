8b8df0d78859c457498d761334adb47c3808789c
Masoud Ghorbani <contact@mgh.space>
2024-12-01 23:46:29 +0100

    test: refactor config tests to use temporary config file and improve error handling

--- src/providers/config.test.ts
+++ src/providers/config.test.ts
@@ -1,41 +1,26 @@
-import { ConfigSchema, safeLoadConfig } from './config'
-
 import { expect } from 'chai'
-import mock from 'mock-fs'
-import { OVM_CONFIG_FILENAME } from '../utils/constants'
+import { destroyConfigMockFile, getTmpConfigFilePath } from '../utils/testing'
+import { ConfigSchema, createDefaultConfig, safeLoadConfig } from './config'
+
+const tmpConfigFilePath = getTmpConfigFilePath()
 
 describe('Config', () => {
+  beforeEach(() => {
+    destroyConfigMockFile(tmpConfigFilePath)
+  })
   it("should load config from user's home dir", async () => {
-    const userHome = '/home/user'
-    const configPath = `${userHome}/${OVM_CONFIG_FILENAME}`
-
     const sampleDefaultConfig = ConfigSchema.parse({})
-
-    mock({
-      [userHome]: {
-        [OVM_CONFIG_FILENAME]: JSON.stringify(sampleDefaultConfig),
-      },
-    })
-
-    const config = await safeLoadConfig(configPath)
+    await createDefaultConfig(tmpConfigFilePath)
+    const config = await safeLoadConfig(tmpConfigFilePath)
 
     expect(config.success).to.be.true.equal(true)
     expect(config.error).to.be.undefined.equal(undefined)
     expect(config.data).to.deep.equal(sampleDefaultConfig)
-    mock.restore()
   })
 
   it('should throw an error if the config file is invalid', async () => {
-    const userHome = '/home/user'
-    const configPath = `${userHome}/${OVM_CONFIG_FILENAME}`
-    mock({
-      [userHome]: {
-        [OVM_CONFIG_FILENAME]: 'invalid-json',
-      },
-    })
-
     try {
-      const { error } = await safeLoadConfig(configPath)
+      const { error } = await safeLoadConfig('non-existent-file')
       throw error
     } catch (error) {
       expect(error).to.be.an('error')
